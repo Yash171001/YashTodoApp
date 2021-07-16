01ff9a36c5f1b0532525002fc7a402c4c3a9739e
Yash <yash.sharma171001@gmail.com>
2021-07-16 20:53:30 +0530

    First commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            this is about MyComponents
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident nobis ratione ut repudiandae dolore eaque accusantium omnis est. Inventore expedita deserunt dolorem sunt ipsum sequi quos, tempora quibusdam ducimus placeat! Excepturi, necessitatibus aliquam?
+            </p>
+        </div>
+    )
+}
